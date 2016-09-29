ba3a33f1e02a5026c210277331ad5b845ef1f9c5
cmhoove14 <cmhoove14@gmail.com>
2016-09-29 15:24:08 -0700

    Cleaned up some redundancy and added code to create hh data summary on the village level (fyi, this is where I discovered fault in hh-dataset compilation that was introducing NAs, because number of people surveyed ended up being larger than number of records in the individual level dataset

--- HH_explore.R
+++ HH_explore.R
@@ -11,8 +11,6 @@ data<-read.csv("C:/Users/chris_hoover/Documents/RemaisWork/SurfaceH2O/Ecuador/In
 gis<-read.csv("C:/Users/chris_hoover/Documents/RemaisWork/SurfaceH2O/Ecuador/IntraVillage/GIS_ID_Casa.csv")  
   gishhs<-unique(gis$ID_Casa)
   giscols<-colnames(gis)
-#projection to use
-  prj<-CRS("+proj=utm +zone=17 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
 
 #merge spatial data with surveillance data; looks like we lose 6125 records
 hh2<-merge(x=hh, y=gis, by.x="HH", by.y="ID_Casa")
@@ -33,16 +31,16 @@ hh2<-hh2[, !names(hh2) %in% giscols[!giscols %in% c("Com", "Numero", "Ocupada", 
   hh2$d_inc = (hh2$inc_diarrhea / hh2$n_drisk2)
 
 #Check out distribution of incidence values  
-  plot(hist(hh2$d_inc))                        
-  plot(hist(log(hh2$d_inc+1)))
-  plot(hist(log(hh2$d_inc[hh2$d_inc > 0]+1)))
+  hist(hh2$d_inc)                        
+  hist(log(hh2$d_inc+1))
+  hist(log(hh2$d_inc[hh2$d_inc > 0]+1))
   
 #get household total number of cases  
   hh2_totals<-as.numeric()
   for(i in 1:length(hh2s)){
     hh2_totals[i] = sum(hh2$inc_diarrhea[hh2$HH == hh2s[i]])
   }
-  plot(hist(hh2_totals)) 
+  hist(hh2_totals)
   plot(x = hh2s, y = hh2_totals)
   
 #A handful of houses seem to be way more infected than others, let's check them out 
@@ -55,10 +53,55 @@ hh2<-hh2[, !names(hh2) %in% giscols[!giscols %in% c("Com", "Numero", "Ocupada", 
   plot(hist(log(hh2_totals[hh2_totals <= 150]+1)))
   
 #Scatterplot matrix of random variables (week, hh, village, x/y coord)
-  pairs(hh2[,c(1,3:5,36:38)])
+  pairs(hh2[,c(1,3:5,36:38)], cex=0.6, col='grey50')
   
 #Scatterplot matrix of age and gender variables
   pairs(hh2[,c(7:13,38)], cex=0.6, col ='blue')
   
 #Scatterplot matrix of WASH and SES variables
   pairs(hh2[,c(20:29,38)], cex=0.6, col ='red')
+  
+#Make dataset to import into ArcGIS
+  h<-unique(hh2$HH)
+  
+  fin<-data.frame(hh = h,
+                  com = 0,
+                  n_surv = 0,
+                  n_total = 0,
+                  d_total = 0,
+                  d_per_n = 0,
+                  f_total = 0,
+                  point_x = 0,
+                  point_y = 0)
+  
+  for(i in 1:nrow(fin)){
+    fin[i,2] = gis$Com[gis$ID_Casa == fin$hh[i]][1]
+    fin[i,3] = length(hh2$HH[hh2$HH == fin$hh[i]])
+    fin[i,4] = sum(hh2$n_people[hh2$HH == fin$hh[i]])
+    fin[i,5] = sum(hh2$inc_diarrhea[hh2$HH == fin$hh[i]])
+    fin[i,6] = sum(hh2$inc_diarrhea[hh2$HH == fin$hh[i]]) / 
+               sum(hh2$n_people[hh2$HH == fin$hh[i]])
+    fin[i,7] = sum(hh2$inc_fever[hh2$HH == fin$hh[i]])
+    fin[i,8] = gis$POINT_X[gis$ID_Casa == fin$hh[i]][1]
+    fin[i,9] = gis$POINT_Y[gis$ID_Casa == fin$hh[i]][1]
+  }
+  
+  write.csv(fin, "C:/Users/chris_hoover/Documents/RemaisWork/SurfaceH2O/Ecuador/IntraVillage/hh_xy_only.csv")
+  
+  coms<-sort(unique(fin$com))
+  coms.sum<-data.frame(vid = 0,
+                       n_hh = 0,
+                       n_hhs = 0,
+                       n_ppls = 0,
+                       n_d = 0)
+  for(i in 1:length(coms)){
+    coms.sum[i,1] = coms[i]
+    coms.sum[i,2] = length(fin$hh[fin$com == coms[i]])
+    coms.sum[i,3] = sum(fin$n_surv[fin$com == coms[i]])
+    coms.sum[i,4] = sum(fin$n_total[fin$com == coms[i]])
+    coms.sum[i,5] = sum(fin$d_total[fin$com == coms[i]])
+  }
+  
+  write.csv(coms.sum, "C:/Users/chris_hoover/Documents/RemaisWork/SurfaceH2O/Ecuador/IntraVillage/com_hh_sum.csv",
+            row.names = FALSE)
+  
