de07ee7968f24cd7f98187ae47c4c75e4eba61db
cmhoove14 <cmhoove14@gmail.com>
2016-10-03 11:33:04 -0700

    Added code to merge HHs that had the same coordinates as found when importing into ArcGIS

--- HH_explore.R
+++ HH_explore.R
@@ -1,5 +1,6 @@
 require(rgdal)
 require(maptools)
+require(plyr)
 
 data<-read.csv("C:/Users/chris_hoover/Documents/RemaisWork/SurfaceH2O/Ecuador/IntraVillage/hh_condense.csv")  
 
@@ -43,10 +44,9 @@ hh2<-hh2[, !names(hh2) %in% giscols[!giscols %in% c("Com", "Numero", "Ocupada", 
   hist(hh2_totals)
   plot(x = hh2s, y = hh2_totals)
   
-#A handful of houses seem to be way more infected than others, let's check them out 
+#A couple of houses seem to be way more infected than others, let's check them out 
   hh2_hi<-subset(hh2, HH == c(hh2s[which(hh2_totals >= 20)]))
-  #All three houses are in village three and have high hh population that appears to be quite variable as
-  #well; maybe something like a hostel that has highly transient population?
+  #both houses have 10+ people, one in Vil17 one in Vil19
   
 #What about distribution without those three hhs?
   hist(hh2_totals[hh2_totals <= 20])
@@ -76,7 +76,7 @@ hh2<-hh2[, !names(hh2) %in% giscols[!giscols %in% c("Com", "Numero", "Ocupada", 
   
   for(i in 1:nrow(fin)){
     fin[i,2] = gis$Com[gis$ID_Casa == fin$hh[i]][1]
-    fin[i,3] = length(hh2$HH[hh2$HH == fin$hh[i]])
+    fin[i,3] = sum(!is.na(hh2$HH[hh2$HH == fin$hh[i]]))
     fin[i,4] = sum(hh2$n_people[hh2$HH == fin$hh[i]])
     fin[i,5] = sum(hh2$inc_diarrhea[hh2$HH == fin$hh[i]])
     fin[i,6] = sum(hh2$inc_diarrhea[hh2$HH == fin$hh[i]]) / 
@@ -87,7 +87,81 @@ hh2<-hh2[, !names(hh2) %in% giscols[!giscols %in% c("Com", "Numero", "Ocupada", 
   }
   
   write.csv(fin, "C:/Users/chris_hoover/Documents/RemaisWork/SurfaceH2O/Ecuador/IntraVillage/hh_xy_only.csv")
+
+#Looks like there are some housholds that have multiple IDs in the dataset ###################
+  #Same XY coordinates, but different HH-IDs
+  dups<-duplicated(fin[,c(8,9)]) #Find duplicated xy coords
+    
+  dupsy<-duplicated(fin[,9])     #check if can just use y coords, yes (only duplicated xy coord have duplicated y coords)
+    ys<-fin[dupsy,9]  
+  duphhs<-fin$hh[fin$point_y %in% ys]
+    fin2<-subset(fin, hh %in% duphhs)
+
+  #Save this data frame to show to people who know more about the data
+    write.csv(fin2, "C:/Users/chris_hoover/Documents/RemaisWork/SurfaceH2O/Ecuador/IntraVillage/hh_xy_duplicates.csv", row.names=FALSE)
   
+  #condense duplicated hhs into the lowest hh id    
+    dupids<-as.numeric()
+    dup1<-as.numeric()
+    dup2<-as.numeric()
+    dup3<-as.numeric()
+    
+    for(i in 1:length(ys)){
+      print(fin2$hh[fin2$point_y == ys[i]])            #Household groups that are duplicated
+      dup1[i] = fin2$hh[fin2$point_y == ys[i]][1]      #hh id of first duplicate
+      dup2[i] = fin2$hh[fin2$point_y == ys[i]][2]      #hh id of first duplicate
+      dup3[i] = fin2$hh[fin2$point_y == ys[i]][3]      #hh id of first duplicate
+      dupids[i] = sum(fin2$hh[fin2$point_y == ys[i]])  #hh id that would result if two data frames were added
+    }
+  
+  med1<-subset(fin, !hh %in% duphhs)  
+  med2<-data.frame(hh = 0,
+                   com = 0,
+                   n_surv = 0,
+                   n_total = 0,
+                   d_total = 0,
+                   d_per_n = 0,
+                   f_total = 0,
+                   point_x = 0,
+                   point_y = 0)
+  
+  for(i in 1:length(dup1)){
+    med2[i,1] = dup1[i]
+    med2[i,2] = gis$Com[gis$ID_Casa == dup1[i]][1]
+    med2[i,3] = sum(!is.na(hh2$HH[hh2$HH == dup1[i]]),
+                    !is.na(hh2$HH[hh2$HH == dup2[i]]),
+                    !is.na(hh2$HH[hh2$HH == dup3[i]]))
+    med2[i,4] = sum(hh2$n_people[hh2$HH == dup1[i]],
+                    hh2$n_people[hh2$HH == dup2[i]],
+                    hh2$n_people[hh2$HH == dup3[i]],
+                    na.rm = TRUE)
+    med2[i,5] = sum(hh2$inc_diarrhea[hh2$HH == dup1[i]],
+                    hh2$inc_diarrhea[hh2$HH == dup2[i]],
+                    hh2$inc_diarrhea[hh2$HH == dup3[i]],
+                    na.rm = TRUE)
+    med2[i,6] = sum(hh2$inc_diarrhea[hh2$HH == dup1[i]],
+                    hh2$inc_diarrhea[hh2$HH == dup2[i]],
+                    hh2$inc_diarrhea[hh2$HH == dup3[i]],
+                    na.rm = TRUE) / 
+                sum(hh2$n_people[hh2$HH == dup1[i]],
+                    hh2$n_people[hh2$HH == dup2[i]],
+                    hh2$n_people[hh2$HH == dup3[i]],
+                    na.rm = TRUE)
+    med2[i,7] = sum(hh2$inc_fever[hh2$HH == dup1[i]],
+                    hh2$inc_fever[hh2$HH == dup2[i]],
+                    hh2$inc_fever[hh2$HH == dup3[i]],
+                    na.rm = TRUE)
+    med2[i,8] = gis$POINT_X[gis$ID_Casa == dup1[i]][1]
+    med2[i,9] = gis$POINT_Y[gis$ID_Casa == dup1[i]][1]
+  }
+  
+  med2<-med2[-6,]
+  
+  finfin<-rbind(med1, med2)
+  
+  write.csv(finfin, "C:/Users/chris_hoover/Documents/RemaisWork/SurfaceH2O/Ecuador/IntraVillage/hh_xy_only_dups_rmv.csv")
+  
+#Get summary data for each community on the hh level ###################    
   coms<-sort(unique(fin$com))
   coms.sum<-data.frame(vid = 0,
                        n_hh = 0,
@@ -96,7 +170,7 @@ hh2<-hh2[, !names(hh2) %in% giscols[!giscols %in% c("Com", "Numero", "Ocupada", 
                        n_d = 0)
   for(i in 1:length(coms)){
     coms.sum[i,1] = coms[i]
-    coms.sum[i,2] = length(fin$hh[fin$com == coms[i]])
+    coms.sum[i,2] = sum(!is.na(fin$hh[fin$com == coms[i]]))
     coms.sum[i,3] = sum(fin$n_surv[fin$com == coms[i]])
     coms.sum[i,4] = sum(fin$n_total[fin$com == coms[i]])
     coms.sum[i,5] = sum(fin$d_total[fin$com == coms[i]])
